bfc9b28003d043ef08c42ed893a125502aea97e8
poi519 <iamyou519@mail.ru>
2013-09-01 23:43:59 +0300

    Moved update() method to Movable template class

---
+++ src/com/example/Pacman/Movable.java
@@ -0,0 +1,79 @@
+package com.example.Pacman;
+
+abstract class Movable {
+    abstract void tryMoveInNewCell(float newX, float newY);
+    abstract void updateWhileStandingStill();
+
+    private float x;
+    private float y;
+    private Direction direction;
+    private float speed;
+    private boolean moving;
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    protected void setDirection(Direction direction) {
+       this.direction = direction;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    final public void update() {
+        float dr = speed * 1f / 60f;
+        float newX = x;
+        float newY = y;
+        if(moving) {
+            switch(direction) {
+                case UP:
+                    newY = y - dr; break;
+                case DOWN:
+                    newY = y + dr; break;
+                case LEFT:
+                    newX = x - dr; break;
+                case RIGHT:
+                    newX = x + dr; break;
+            }
+            if(((int) newX != (int) x
+                    || (int) newY != (int) y)) {     //passed through a cell center;
+                tryMoveInNewCell(newX, newY);
+            } else {
+                x = newX;
+                y = newY;
+            }
+        } else {
+            updateWhileStandingStill();
+        }
+    }
+}
--- src/com/example/Pacman/Pacman.java
+++ src/com/example/Pacman/Pacman.java
@@ -35,13 +35,8 @@ enum Direction {
 
 }
 
-public class Pacman  implements HasRadius {
-    private float x;
-    private float y;
-    private Direction direction;
+public class Pacman extends Movable implements HasRadius {
     private Direction scheduledDirection;
-    private float speed;
-    private boolean moving;
 
     public Direction getScheduledDirection() {
         return scheduledDirection;
@@ -51,50 +46,8 @@ public class Pacman  implements HasRadius {
         this.scheduledDirection = scheduledDirection;
     }
 
-
-    public boolean isMoving() {
-        return moving;
-    }
-
-    public void setMoving(boolean moving) {
-        this.moving = moving;
-    }
-
     public float getRadius() {
         return 0.5f;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    private void setDirection(Direction direction) {
-        //Log.d("setDirection", "direction set to" + direction.toString());
-        this.direction = direction;
-    }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
     }
 
     public Pacman(int xx, int yy){
@@ -138,53 +91,28 @@ public class Pacman  implements HasRadius {
         }
     }
 
-    public void update() {
-        float dr = getSpeed() * 1f / 60f;
-        float newX = getX();
-        float newY = getY();
-        if(moving) {
-            switch(getDirection()) {
-                case UP:
-                    newY = getY() - dr; break;
-                case DOWN:
-                    newY = getY() + dr; break;
-                case LEFT:
-                    newX = getX() - dr; break;
-                case RIGHT:
-                    newX = getX() + dr; break;
-            }
-            if(((int) newX != (int) x
-                || (int) newY != (int) y)) {     //passed through a cell center;
-                tryMoveInNewCell(newX, newY);
-            } else {
-                x = newX;
-                y = newY;
-            }
-        } else {
-            updateWhileStandingStill();
-        }
-    }
-
+    @Override
     public void tryMoveInNewCell(float newX, float newY) {
         eat();
         if(getScheduledDirection() != getDirection()    //direction change was scheduled
            && tryChangeDirection()) {                   //and happened
-            return;
+            //Do nothing
         } else {
             //Check for wall
-            float[] nextCell = direction.nextCell(newX, newY);
+            float[] nextCell = getDirection().nextCell(newX, newY);
             Location nextLocation = Game.getInstance().getMap().getLocation(nextCell);
             if(nextLocation == Location.Wall) {
-                moving = false;
-                x = Math.round(newX);
-                y = Math.round(newY);
+                setMoving(false);
+                setX(Math.round(newX));
+                setY(Math.round(newY));
             } else {
-                x = newX;
-                y = newY;
+                setX(newX);
+                setY(newY);
             }
         }
     }
 
+    @Override
     public void updateWhileStandingStill() {
         eat();
     }
